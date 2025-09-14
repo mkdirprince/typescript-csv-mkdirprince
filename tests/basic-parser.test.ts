01463216d5aa241471aa1ee2f5be266301de38fe
mkdirprince <mkdirprince@github.com>
2025-09-14 10:47:55 +0000

    add optional zod validation to the parser and test suits for it

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -1,5 +1,6 @@
 import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
+import { z } from "zod";
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
 const PEOPLE1_CSV_PATH = path.join(__dirname, "../data/people1.csv");
@@ -22,6 +23,7 @@ test("parseCSV yields only arrays", async () => {
   }
 });
 
+// tests for future extensibility
 test("parseCSV handles empty fields or trailing commas", async () => {
   const result = await parseCSV(PEOPLE1_CSV_PATH);
   expect(result[1]).toEqual(["Prince", "", ""]);
@@ -40,4 +42,33 @@ test("parseCSV handles commas in quotes", async () => {
 test("parseCSV handles qoutes in qoutes", async () => {
   const result = await parseCSV(PEOPLE1_CSV_PATH);
   expect(result[5]).toEqual(['Prince is a good "boy"', "28", "wils@gmail.com"]);
+});
+
+// tests for improved parser with introduction of validation
+describe("parseCSV with schema validation", () => {
+  test("parseCSV works for shema with all fields as a string", async () => {
+    const schema = z.tuple([z.string(), z.string()]);
+    const result = await parseCSV(PEOPLE_CSV_PATH, schema);
+
+    // all rows successfully matches schema definition
+    for (const res of result) {
+      expect(res.success).toBe(true);
+    }
+
+    if (result[1].success) {
+      expect(result[3].data).toEqual(["Charlie", "25"]);
+    }
+    if (result[2].success) {
+      expect(result[2].data).toEqual(["Bob", "thirty"]);
+    }
+  });
+
+  test("parseCSV works with age as number", async () => {
+    const schema = z.tuple([z.string(), z.coerce.number()]);
+    const result = await parseCSV(PEOPLE_CSV_PATH, schema);
+
+    expect(result[1].success).toBe(true);
+    expect(result[2].success).toBe(false); // fails for thirty
+    expect(result[3].success).toBe(true);
+  });
 });
